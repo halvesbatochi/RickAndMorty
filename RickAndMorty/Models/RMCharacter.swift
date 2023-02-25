2317e6a57844d41b5b526a85d8d0918cfc21a271
Henrique Alves Batochi <henrique.alvesbatochi@gmail.com>
2023-02-25 00:37:23 -0300

    Create Models

---
+++ RickAndMorty/Models/RMCharacter.swift
@@ -0,0 +1,8 @@
+//
+//  RMCharacter.swift
+//  RickAndMorty
+//
+//  Created by Henrique Alves Batochi on 25/02/23.
+//
+
+import Foundation
---
+++ RickAndMorty/Models/RMEpisode.swift
@@ -0,0 +1,8 @@
+//
+//  RMEpisode.swift
+//  RickAndMorty
+//
+//  Created by Henrique Alves Batochi on 25/02/23.
+//
+
+import Foundation
