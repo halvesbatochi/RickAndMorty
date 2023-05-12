436a687fd114a5044ead75508c826b16576aff7d
Henrique Alves Batochi <henrique.alvesbatochi@gmail.com>
2023-05-12 16:40:55 -0300

    Location Detail Screen

--- RickAndMorty/Controllers/Other/RMLocationDetailViewController.swift
+++ RickAndMorty/Controllers/Other/RMLocationDetailViewController.swift
@@ -7,26 +7,66 @@
 
 import UIKit
 
-final class RMLocationDetailViewController: UIViewController {
+final class RMLocationDetailViewController: UIViewController, RMLocationDetailViewViewModelDelegate, RMLocationDetailViewDelegate {
     
-    private let location: RMLocation
+    private let viewModel: RMLocationDetailViewViewModel
+    private let detailView = RMLocationDetailView()
     
     // MARK: - Init
     
     init(location: RMLocation) {
-        self.location = location
+        let url = URL(string: location.url)
+        self.viewModel = RMLocationDetailViewViewModel(endpointURL: url)
         super.init(nibName: nil, bundle: nil)
     }
     
     required init?(coder: NSCoder) {
-        fatalError("Unsupported")
+        fatalError()
     }
-    
-    // MARK: Lifecycle
+
+    // MARK: - Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.addSubview(detailView)
+        viewModel.delegate = self
+        detailView.delegate = self
+        addConstraints()
         title = "Location"
-        view.backgroundColor = .systemBackground
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(didTapShare))
+        
+        viewModel.delegate = self
+        viewModel.fetchLocationData()
     }
+    
+    private func addConstraints() {
+        NSLayoutConstraint.activate([
+            detailView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            detailView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor),
+            detailView.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor),
+            detailView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+        ])
+    }
+    
+    @objc
+    private func didTapShare() {
+        
+    }
+    
+    // MARK: - View Delegate
+    
+    func rmEpisodeDetailView(_ detailView: RMLocationDetailView, didSelect character: RMCharacter) {
+        let vc = RMCharacterDetailViewController(viewModel: .init(character: character))
+        vc.title = character.name
+        vc.navigationItem.largeTitleDisplayMode = .never
+        navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    // MARK: - ViewModel Delegate
+    
+    func didFetchLocationDetails() {
+        detailView.configure(with: viewModel)
+    }
+
 }
