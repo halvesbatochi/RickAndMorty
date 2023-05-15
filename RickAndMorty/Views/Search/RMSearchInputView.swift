92287fbc9f3777b8a10d42e5d06b51cc125bad47
Henrique Alves Batochi <henrique.alvesbatochi@gmail.com>
2023-05-15 15:16:25 -0300

    Search Bar

--- RickAndMorty/Views/Search/RMSearchInputView.swift
+++ RickAndMorty/Views/Search/RMSearchInputView.swift
@@ -8,5 +8,55 @@
 import UIKit
 
 final class RMSearchInputView: UIView {
+    
+    private let searchBar: UISearchBar = {
+        let searchBar = UISearchBar()
+        searchBar.placeholder = "Search"
+        searchBar.translatesAutoresizingMaskIntoConstraints = false
+        return searchBar
+    }()
+    
+    private var viewModel: RMSearchInputViewViewModel? {
+        didSet {
+            guard let viewModel = viewModel, viewModel.hasDynamicOptions else {
+                return
+            }
+            let options = viewModel.options
+            createOptionSelectionViews(options: options)
+        }
+    }
+    
+    // MARK: - Init
 
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        translatesAutoresizingMaskIntoConstraints = false
+        backgroundColor = .systemCyan
+        addSubviews(searchBar)
+        addConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func addConstraints() {
+        NSLayoutConstraint.activate([
+            searchBar.topAnchor.constraint(equalTo: topAnchor),
+            searchBar.leftAnchor.constraint(equalTo: leftAnchor),
+            searchBar.rightAnchor.constraint(equalTo: rightAnchor),
+            searchBar.heightAnchor.constraint(equalToConstant: 58)
+        ])
+    }
+    
+    private func createOptionSelectionViews(options: [RMSearchInputViewViewModel.DynamicOption]) {
+        for option in options {
+            print(option.rawValue)
+        }
+    }
+    
+    public func configure(with viewModel: RMSearchInputViewViewModel) {
+        searchBar.placeholder = viewModel.searchPlaceholderText
+        self.viewModel = viewModel
+    }
 }
