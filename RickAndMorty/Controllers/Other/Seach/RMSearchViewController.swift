dd27808147101eb4fec52455721763f49681ed06
Henrique Alves Batochi <henrique.alvesbatochi@gmail.com>
2023-05-17 15:32:30 -0300

    Search Option Bottom Sheet

--- RickAndMorty/Controllers/Other/Seach/RMSearchViewController.swift
+++ RickAndMorty/Controllers/Other/Seach/RMSearchViewController.swift
@@ -93,7 +93,12 @@ final class RMSearchViewController: UIViewController {
 
 extension RMSearchViewController: RMSearchViewDelegate {
     func rmSearchView(_ searchView: RMSearchView, didSelectOption option: RMSearchInputViewViewModel.DynamicOption) {
-        print("Hi")
+        let vc = RMSearchOptionPickerViewController(option: option) { selection in
+            print("Here")
+        }
+        vc.sheetPresentationController?.detents = [.medium()]
+        vc.sheetPresentationController?.prefersGrabberVisible = true
+        present(vc, animated: true)
     }
     
     
