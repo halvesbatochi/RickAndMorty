fc5c38c8f8284bd37cd4ce0cb1d78947f6eec895
Henrique Alves Batochi <henrique.alvesbatochi@gmail.com>
2023-05-26 02:45:51 -0300

    Location Pagination

---
+++ RickAndMorty/Views/Location/RMTableLoadingFooterView.swift
@@ -0,0 +1,39 @@
+//
+//  RMTableLoadingFooterView.swift
+//  RickAndMorty
+//
+//  Created by Henrique Alves Batochi on 26/05/23.
+//
+
+import UIKit
+
+final class RMTableLoadingFooterView: UIView {
+    
+    private let spinner: UIActivityIndicatorView = {
+        let spinner = UIActivityIndicatorView()
+        spinner.translatesAutoresizingMaskIntoConstraints = false
+        spinner.hidesWhenStopped = true
+        return spinner
+    }()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        addSubview(spinner)
+        spinner.startAnimating()
+        
+        addConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("Unsupported")
+    }
+    
+    private func addConstraints() {
+        NSLayoutConstraint.activate([
+            spinner.widthAnchor.constraint(equalToConstant: 55),
+            spinner.heightAnchor.constraint(equalToConstant: 55),
+            spinner.centerXAnchor.constraint(equalTo: centerXAnchor),
+            spinner.centerYAnchor.constraint(equalTo: centerYAnchor)
+        ])
+    }
+}
